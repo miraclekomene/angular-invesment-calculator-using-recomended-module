d58a991d26742ae84fece9ce2e59eca580731ccc
miraclekomene <miraclekomene15@gmail.com>
2025-08-12 11:29:12 +0100

    investment

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { UserInputModule } from "./user-input/user-input.module";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        InvestmentResultsComponent,
+    ],
+    imports: [UserInputModule, BrowserModule],//"BrowserModule" This module will actually include all those pipes,CurrencyPipe,NgIf and NgFor directives,.
+    bootstrap: [AppComponent],
+})
+export class AppModule{
+    
+}
